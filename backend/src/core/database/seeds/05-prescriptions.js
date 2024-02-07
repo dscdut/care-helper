987e616973a2aadd7cd199a75c577ef529ec94d6
hungpham28 <hungpham28092003@gmail.com>
2024-02-07 11:44:26 +0700

    fix: add 1000 row examinations and 1200 row prescriptions, medical tests

--- backend/src/core/database/seeds/05-prescriptions.js
+++ backend/src/core/database/seeds/05-prescriptions.js
@@ -15,17 +15,36 @@ const readJsonFile = async filePath => {
     }
 };
 const tableName = 'prescriptions';
-const numPrescriptions = 45; // Adjust the number of prescriptions you want to generate
+const numPrescriptions = 1200; // Adjust the number of prescriptions you want to generate
 
 exports.seed = async knex => {
     await knex(tableName).del();
     const drugsList = await readJsonFile(drugsFilePath);
+
+    // Evenly distribute values
+    let shuffledExaminationIds = Array.from(
+        {
+            length: Math.floor(
+                (numExaminations / numPrescriptions) * numExaminations,
+            ),
+        },
+        (_, index) => ((index + 1) % numExaminations !== 0 ? ((index + 1) % numExaminations) : numExaminations),
+    );
+    shuffledExaminationIds = shuffledExaminationIds.concat(
+        numPrescriptions - shuffledExaminationIds.length > 0
+            ? Array.from(
+                { length: numPrescriptions - shuffledExaminationIds.length },
+                (_, index) => fakerVI.number.int({ min: 1, max: numExaminations }),
+            )
+            : [],
+    );
 
     const prescriptions = Array.from({ length: numPrescriptions }, () => {
         const startDate = fakerVI.date.between({
             from: fakerVI.date.past(),
             to: fakerVI.date.future(),
         });
+
         const endDate = fakerVI.date.between({
             from: startDate,
             to: new Date(startDate.getTime() + 90 * 24 * 60 * 60 * 1000), // 90 days
@@ -34,18 +53,17 @@ exports.seed = async knex => {
             start_date: startDate,
             end_date: endDate,
             details: JSON.stringify(
-                fakerVI.helpers.arrayElements(drugsList, { min: 1, max: 10 }).map(e => ({
-                    medicineName: e,
-                    usage: fakerVI.lorem.sentence({ max: 10 }),
-                    quantity: `${fakerVI.number.int({ max: 300 })} viên`,
-                })),
+                fakerVI.helpers
+                    .arrayElements(drugsList, { min: 1, max: 10 })
+                    .map(e => ({
+                        medicineName: e,
+                        usage: fakerVI.lorem.sentence({ max: 10 }),
+                        quantity: `${fakerVI.number.int({ max: 300 })} viên`,
+                    })),
             ),
             note: fakerVI.lorem.sentence(),
             prescription_filename: fakerVI.image.urlLoremFlickr(),
-            examination_id: fakerVI.number.int({
-                min: 1,
-                max: numExaminations,
-            }), // Assuming examinations are already seeded
+            examination_id: shuffledExaminationIds.splice(shuffledExaminationIds.length * Math.random() || 0, 1)[0], // Assuming examinations are already seeded
             created_at: fakerVI.date.past({ years: 10 }),
             updated_at: fakerVI.date.recent(),
         };
